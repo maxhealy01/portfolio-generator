e4e10e4ac882f786a18b2b96eba2c5e2ffad6fa5
Max Healy <maxhealy01@gmail.com>
2020-11-19 17:03:21 -0600

    Finished functions to generate HTML

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-
-
-// const generatePage = require('./src/page-template.js')
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
 
 // const pageHTML = generatePage(name, github);
 
@@ -141,5 +139,9 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData)
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+     });
   })
